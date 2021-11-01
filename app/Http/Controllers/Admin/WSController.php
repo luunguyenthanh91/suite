19fd72b4b40bef37f27e680540f3553cc6f1f7b5
LuuNguyen <luunguyenthanh91@gmail.com>
2021-11-02 03:33:18 +0900

    ff

--- app/Http/Controllers/Admin/WSController.php
+++ app/Http/Controllers/Admin/WSController.php
@@ -1148,7 +1148,7 @@ class WSController extends Controller
             $approved_by_sign = $approved_user->sign_name;
         }
 
-        $file_name = trans('label.worksheet_pdf2')."_".$ws->month."_".$employee->code."(".$employee->name.")";
+        $file_name = $ws->month."_".trans('label.worksheet_pdf2')."_".$employee->code."(".$employee->name.")";
         
 
         $worktimecount = $this->CalculateTime2($worktimelist);
