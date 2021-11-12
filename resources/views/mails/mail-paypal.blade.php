7e4d35f63ace1a36f3e2249de19ad036321d0f9f
LuuNguyen <luunguyenthanh91@gmail.com>
2021-11-13 08:28:10 +0900

    u

--- resources/views/mails/mail-paypal.blade.php
+++ resources/views/mails/mail-paypal.blade.php
@@ -1,1 +1,13 @@
-123
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mail Title From onlinewebtutorblog.com</title>
+</head>
+<body>
+    <h1>{{ $title }}</h1>
+    <p>{{ $body }}</p>
+     
+    <p>Thanks, Online Web Tutor Support Team</p>
+</body>
+</html>
+  
