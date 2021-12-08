d12dbee2255fffb842034f471e9fa4578639c70d
LuuNguyen <luunguyenthanh91@gmail.com>
2021-12-08 12:05:46 +0900

    gg

--- resources/views/admin/payslip-pdf.blade.php
+++ resources/views/admin/payslip-pdf.blade.php
@@ -96,7 +96,7 @@
                             <td class="headerColor border-all3 title_form" style="height:35px;vertical-align:middle;text-align:center;background:#EEEEEE;">{{ trans('label.plus') }}</td>
                         </tr>
                         <tr>
-                            <td class="border-all3" style="height:442px;vertical-align:top;">
+                            <td class="border-all3" style="height:443px;vertical-align:top;">
                                 <table style='width:100%;border-collapse:collapse;font-size:10.5;border:0px'>
                                     <tr>
                                         <td class="headerColor border-all31 title_form" style="padding-left:10px;height:30px;width:100px;vertical-align:middle;background-color:#FAFAFA !important;">{{ trans('label.kihonkyu') }}</td>
@@ -120,7 +120,7 @@
                                         <td class="headerColor border-all31 title_form" style="height:26px;width:120px;vertical-align:middle;text-align:center;background:#FAFAFA;">
                                         {{ trans('label.plus_zei_total') }}
                                         </td>
-                                        <td class="title_form border-all31 " style="height:26px;vertical-align:middle;text-align:center;padding-right:10px;width:150px !important;;">
+                                        <td class="title_form border-all31 " style="height:26px;vertical-align:middle;text-align:center;padding-right:10px;;">
                                         {{ number_format($data->plus_zei_total) }} 円
                                         </td>
                                     </tr>
@@ -128,7 +128,7 @@
                                         <td class="headerColor border-all31 title_form" style="height:26px;width:120px;vertical-align:middle;text-align:center;background:#FAFAFA;">
                                         {{ trans('label.plus_nozei_total') }}
                                         </td>
-                                        <td class="title_form border-all31" style="height:26px;vertical-align:middle;text-align:center;padding-right:10px;width:150px !important;;">
+                                        <td class="title_form border-all31" style="height:26px;vertical-align:middle;text-align:center;padding-right:10px;;">
                                         {{ number_format($data->plus_nozei_total) }} 円
                                         </td>
                                     </tr>
@@ -136,7 +136,7 @@
                                         <td class="headerColor title_form" style="height:26px;width:120px;vertical-align:middle;text-align:center;background:#FAFAFA;">
                                         {{ trans('label.total_plus') }}
                                         </td>
-                                        <td class="title_form" style="height:26px;vertical-align:middle;text-align:center;padding-right:10px;width:150px !important;;">
+                                        <td class="title_form" style="height:26px;vertical-align:middle;text-align:center;padding-right:10px;;">
                                         {{ number_format($data->plus_total) }} 円
                                         </td>
                                     </tr>
