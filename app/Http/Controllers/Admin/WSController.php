93d087cd91de83b55a7817a5436095c51b75a3d7
LuuNguyen <luunguyenthanh91@gmail.com>
2022-04-04 11:44:01 +0900

    ff

--- app/Http/Controllers/Admin/WSController.php
+++ app/Http/Controllers/Admin/WSController.php
@@ -836,7 +836,7 @@ class WSController extends Controller
                 }
             if ($historyLog2) {
                     $endtime = $historyLog2->time;
-                    $enddate = $historyLog->date;
+                    $enddate = $historyLog2->date;
                 }
             }
 
